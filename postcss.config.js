2c853efddb299e0f4925e0476c9202ec29015915
kailong321200875 <321200875@qq.com>
2024-01-15 16:01:18 +0800

    chore: 支持新语法

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  plugins: [
-    'postcss-preset-env'
-  ]
+  plugins: ['postcss-preset-env']
 }
--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,3 +1,15 @@
 import './index.less'
 
 console.log(process.env)
+
+const addNumber = (a, b) => {
+  return a + b
+}
+const test = undefined
+
+console.log(test ?? '222')
+const object = {
+  name: 'zhangsan'
+}
+console.log(object?.name)
+console.log(addNumber(1, 2))
